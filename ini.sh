28346bfc21afddef4d4f80882a466e77e67d76f7
foostan <foostan27@gmail.com>
2013-08-22 23:09:09 +0900

    add molokai as submodule

--- ini.sh
+++ ini.sh
@@ -2,7 +2,12 @@
 DOTFILE_PATH=$(cd $(dirname $0) && pwd)
 
 # update submodules
-git submodule foreach 'git checkout master; git pull'
+git submodule init
+git submodule update
+#git submodule foreach 'git checkout master; git pull'
+
+# set up vim files
+ln -sf $DOTFILE_PATH/.vim/colors/molokai/colors/molokai.vim $DOTFILE_PATH/.vim/colors/molokai.vim
 
 # set up oh-my-zsh files
 ln -sf $DOTFILE_PATH/ohmyzsh_myfiles/my.zsh .oh-my-zsh/custom/my.zsh
