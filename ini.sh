71078bbe40d1c6481ae2c59e5cfcf356141730bb
foostan <foostan27@gmail.com>
2014-07-14 00:27:40 +0900

    Update dotfiles

--- ini.sh
+++ ini.sh
@@ -11,6 +11,7 @@ ln -sf $DOTFILE_PATH/.vim/colors/molokai/colors/molokai.vim $DOTFILE_PATH/.vim/c
 # set up oh-my-zsh files
 ln -sf $DOTFILE_PATH/ohmyzsh_myfiles/my.zsh .oh-my-zsh/custom/my.zsh
 ln -sf $DOTFILE_PATH/ohmyzsh_myfiles/my.zsh-theme .oh-my-zsh/themes/my.zsh-theme
+mv .oh-my-zsh/lib/termsupport.zsh .oh-my-zsh/lib/termsupport.zsh.del
 
 # set up dotfiles
 for dotfile in .?*
@@ -20,3 +21,6 @@ do
         ln -sf "$DOTFILE_PATH/$dotfile" $HOME
     fi
 done
+
+# todo: install go
+# todo: install peco
