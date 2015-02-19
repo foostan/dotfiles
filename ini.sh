1371c7816f3852bdccc16f4cc42c9b55987346e7
foostan <ks@fstn.jp>
2015-02-20 01:46:49 +0900

    Swap default custom directory to mine

--- ini.sh
+++ ini.sh
@@ -9,9 +9,8 @@ git submodule update
 ln -sf $DOTFILE_PATH/.vim/colors/molokai/colors/molokai.vim $DOTFILE_PATH/.vim/colors/molokai.vim
 
 # set up oh-my-zsh files
-ln -sf $DOTFILE_PATH/ohmyzsh_myfiles/my.zsh .oh-my-zsh/custom/my.zsh
-ln -sf $DOTFILE_PATH/ohmyzsh_myfiles/my.zsh-theme .oh-my-zsh/themes/my.zsh-theme
-mv .oh-my-zsh/lib/termsupport.zsh .oh-my-zsh/lib/termsupport.zsh.del
+rm -rf $DOTFILE_PATH/oh-my-zsh/custom
+ln -sf $DOTFILE_PATH/oh-my-zsh/custom .oh-my-zsh/custom
 
 # set up dotfiles
 for dotfile in .?*
