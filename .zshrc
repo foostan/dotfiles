513326881c46408c2057afae9ab93a6963bd8758
foostan <ks@fstn.jp>
2015-02-20 02:35:02 +0900

    Create peco-history plugin

--- .zshrc
+++ .zshrc
@@ -7,6 +7,7 @@ plugins=(
     # custom
     #==============================
     enter
+    peco-history
 
     #==============================
     # official
--- oh-my-zsh/custom/my.zsh
+++ oh-my-zsh/custom/my.zsh
@@ -33,21 +33,6 @@ setopt share_history # share command history data
 autoload history-search-end
 ## install peco before initialize zsh
 ## go get github.com/peco/peco/cmd/peco
-function peco-select-history() {
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-    BUFFER=$(\history -n 1 | \
-        eval $tac | \
-        peco --query "$LBUFFER")
-    CURSOR=$#BUFFER
-    zle clear-screen
-}
-zle -N peco-select-history
-bindkey '^r' peco-select-history
 
 #==============================
 # Alias
